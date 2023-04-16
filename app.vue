d3bb469e2285e8c3eb3f47acb34c6067e415ab88
hoda ghaffari <edu.ghaffari@gmail.com>
2023-04-16 11:33:24 +0430

    15 an example of grid

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <NuxtPage>
+    </NuxtPage>
+  </div>
+</template>
+<script setup>
+useHead({
+  //htmlAttrs: { dir: 'rtl', lang: 'fa' },
+  htmlAttrs: { dir: 'rtl' },
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="bg-gray-400 h-screen">
+        <div class="container mx-auto xl:max-w-screen-xl">
+            <div class="grid grid-cols-1 gap-y-4 md:grid-cols-12 md:gap-8 grid-rows-[60px_minmax(500px,_1fr)]">
+                <div class="bg-white p-4 hidden md:block col-span-2 row-span-2">sidbar</div>
+                <div class="bg-white p-4 col-span-10">sort header</div>
+                <div class="bg-white p-4 col-span-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+                    quae alias ipsum nulla sunt voluptate sit ipsa aut, odio harum tempora possimus perferendis quasi libero
+                    animi. At animi similique ducimus quia quis nisi corporis cumque quibusdam nulla reiciendis aperiam
+                    nesciunt quaerat obcaecati, qui quod fugiat asperiores, totam sed accusantium possimus eaque natus nihil
+                    odio veritatis? Labore culpa recusandae earum perspiciatis quod explicabo in vel quasi magnam. Natus,
+                    corrupti excepturi deleniti omnis autem amet voluptatibus dicta, necessitatibus, aut exercitationem sit
+                    adipisci ullam similique facere veniam placeat? Similique ea atque iure odit accusantium voluptatem a
+                    nobis nihil maxime. Illum dignissimos porro vel?</div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+</script>
